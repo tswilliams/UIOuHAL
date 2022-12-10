ef3d6d2b82b9aaf19b858905be16a592b1ac0558
Alp Akpinar <aakpinar@bu.edu>
2022-12-10 11:46:28 -0500

    Add boost program CLI options

--- example/src/common/main_for_tom.cpp
+++ example/src/common/main_for_tom.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 
+#include <boost/program_options.hpp>
+
 #include "uhal/uhal.hpp"
 
 // Default XML connection file to be used
@@ -11,14 +13,60 @@
 #define DEFAULT_REMOTE_REGISTER "CM_V_INFO.GIT_HASH_1" 
 #define DEFAULT_MMAP_REGISTER   "info.magic"
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])
 {
   using namespace std;
+
+  string connectionFile = DEFAULT_CONNECTION_FILE;
+  string localRegister  = DEFUALT_LOCAL_REGISTER;
+  string remoteRegister = DEFAULT_REMOTE_REGISTER;
+  string mmapRegister   = DEFAULT_MMAP_REGISTER;
+
+  /*
+   * Set up CLI program options.
+   */
+  po::options_description cli_options("CLI options");
+  cli_options.add_options()
+    ("help",  "Help screen")
+    ("cfile"      , po::value<string>(), "XML connection file to use")
+    ("lRegUIO"    , po::value<string>(), "Name of the local UIO register")
+    ("rRegUIO"    , po::value<string>(), "Name of the remote UIO register")
+    ("regMmap"    , po::value<string>(), "Name of the MemMap register")
+  ;
+
+  po::variables_map vmap;        
+  po::store(po::parse_command_line(argc, argv, cli_options), vmap);
+  po::notify(vmap);
+
+  // Display help message and exit.
+  if (vmap.count("help")) {
+    cout << cli_options << endl;
+    return 0;
+  }
+
+  // If CLI options were given, override the defaults set earlier
+  if (vmap.count("cfile")) {
+    connectionFile = vmap["cfile"].as<string>();
+  }
+  if (vmap.count["lRegUIO"]) {
+    localRegister = vmap["lRegUIO"].as<string>();
+  }
+  if (vmap.count["rRegUIO"]) {
+    remoteRegister = vmap["rRegUIO"].as<string>();
+  }
+  if (vmap.count["regMmap"]) {
+    mmapRegister = vmap["regMmap"].as<string>();
+  }
+
+  /*
+   * Launch example program!
+   */
+
   // Connection file to use: Path is relative to the bin/main executable
   // This connection file has two set of hardware interfaces:
   // 1. UIOuHAL interface, 2. MemMap interface
-  string connectionFile(DEFAULT_CONNECTION_FILE);
-
   cout << "Using connection file: " << connectionFile << endl;
 
   // Get the connection manager
@@ -41,21 +89,19 @@ int main(int argc, char* argv[])
   cout << "Got the HW interface for MemMap" << endl;
 
   // Now, read the registers with both interfaces
-  string registerName(DEFUALT_LOCAL_REGISTER);
-  cout << "Trying to read register: " << registerName << " with UIOuHAL interface" << endl;
+  cout << "Trying to read register: " << localRegister << " with UIOuHAL interface" << endl;
   uhal::ValWord<uint32_t> ret_uio;
 
   // UIOuHAL read 
-  ret_uio = hw_uio->getNode(registerName).read();
+  ret_uio = hw_uio->getNode(localRegister).read();
   hw_uio->dispatch();
   cout << "Succesfully read register" << endl;
   cout << "Value: 0x" << hex << ret_uio.value() << endl; 
   
   // 'Remote' register using uIOuHAL
-  string registerNameRemoteUIO(DEFAULT_REMOTE_REGISTER);
-  cout << "Trying to read register: " << registerNameRemoteUIO << " with UIOuHAL interface" << endl;
+  cout << "Trying to read register: " << remoteRegister << " with UIOuHAL interface" << endl;
   try {
-    ret_uio = hw_uio->getNode(registerNameRemoteUIO).read();
+    ret_uio = hw_uio->getNode(remoteRegister).read();
     hw_uio->dispatch();
     cout << "WARNING: NO EXCEPTION THROWN" << endl;
   }
@@ -63,12 +109,12 @@ int main(int argc, char* argv[])
     cout << "Exception caught (as expected): " << e.what() << endl;
   }
 
-  string registerNameMMap(DEFAULT_MMAP_REGISTER);
-  cout << "Trying to read register: " << registerNameMMap << " with MemMap interface" << endl;
+  // Do the MemMap read
+  cout << "Trying to read register: " << mmapRegister << " with MemMap interface" << endl;
   uhal::ValWord<uint32_t> ret_mmap;
 
   // MemMap read, it should go bad here..
-  ret_mmap = hw_mmap->getNode(registerNameMMap).read();
+  ret_mmap = hw_mmap->getNode(mmapRegister).read();
   hw_mmap->dispatch();
   cout << "Succesfully read register" << endl;
   cout << "Value: 0x" << hex << ret_mmap.value() << endl; 
