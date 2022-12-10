fc6d33608eeb58a1752520324f2b18312eea647c
Alp Akpinar <aakpinar@bu.edu>
2022-12-10 11:31:46 -0500

    Use namespace std

--- example/src/common/main_for_tom.cpp
+++ example/src/common/main_for_tom.cpp
@@ -13,64 +13,65 @@
 
 int main(int argc, char* argv[])
 {
+  using namespace std;
   // Connection file to use: Path is relative to the bin/main executable
   // This connection file has two set of hardware interfaces:
   // 1. UIOuHAL interface, 2. MemMap interface
-  std::string connectionFile(DEFAULT_CONNECTION_FILE);
+  string connectionFile(DEFAULT_CONNECTION_FILE);
 
-  std::cout << "Using connection file: " << connectionFile << std::endl;
+  cout << "Using connection file: " << connectionFile << endl;
 
   // Get the connection manager
   uhal::setLogLevelTo(uhal::Notice());
-  uhal::ConnectionManager manager( connectionFile.c_str(), std::vector<std::string>(1,"uioaxi-1.0") ); 
-  std::cout << "Constructed ConnectionManager" << std::endl;
+  uhal::ConnectionManager manager( connectionFile.c_str(), vector<string>(1,"uioaxi-1.0") ); 
+  cout << "Constructed ConnectionManager" << endl;
 
   // Get device interface for UIOuHAL 
-  std::cout << "Getting HW interface for UIOuHAL" << std::endl;
-  std::string connectionFileEntryUIO("test.0");
+  cout << "Getting HW interface for UIOuHAL" << endl;
+  string connectionFileEntryUIO("test.0");
   uhal::HwInterface * hw_uio;
   hw_uio = new uhal::HwInterface(manager.getDevice ( connectionFileEntryUIO.c_str() ));
-  std::cout << "Got the HW interface for UIOuHAL" << std::endl;
+  cout << "Got the HW interface for UIOuHAL" << endl;
 
   // Get device interface for MemMap 
-  std::cout << "Getting HW interface for MemMap" << std::endl;
-  std::string connectionFileEntryMemMap("apollo.c2c.vu7p");
+  cout << "Getting HW interface for MemMap" << endl;
+  string connectionFileEntryMemMap("apollo.c2c.vu7p");
   uhal::HwInterface * hw_mmap;
   hw_mmap = new uhal::HwInterface(manager.getDevice ( connectionFileEntryMemMap.c_str() ));
-  std::cout << "Got the HW interface for MemMap" << std::endl;
+  cout << "Got the HW interface for MemMap" << endl;
 
   // Now, read the registers with both interfaces
-  std::string registerName(DEFUALT_LOCAL_REGISTER);
-  std::cout << "Trying to read register: " << registerName << " with UIOuHAL interface" << std::endl;
+  string registerName(DEFUALT_LOCAL_REGISTER);
+  cout << "Trying to read register: " << registerName << " with UIOuHAL interface" << endl;
   uhal::ValWord<uint32_t> ret_uio;
 
   // UIOuHAL read 
   ret_uio = hw_uio->getNode(registerName).read();
   hw_uio->dispatch();
-  std::cout << "Succesfully read register" << std::endl;
-  std::cout << "Value: 0x" << std::hex << ret_uio.value() << std::endl; 
+  cout << "Succesfully read register" << endl;
+  cout << "Value: 0x" << hex << ret_uio.value() << endl; 
   
   // 'Remote' register using uIOuHAL
-  std::string registerNameRemoteUIO(DEFAULT_REMOTE_REGISTER);
-  std::cout << "Trying to read register: " << registerNameRemoteUIO << " with UIOuHAL interface" << std::endl;
+  string registerNameRemoteUIO(DEFAULT_REMOTE_REGISTER);
+  cout << "Trying to read register: " << registerNameRemoteUIO << " with UIOuHAL interface" << endl;
   try {
     ret_uio = hw_uio->getNode(registerNameRemoteUIO).read();
     hw_uio->dispatch();
-    std::cout << "WARNING: NO EXCEPTION THROWN" << std::endl;
+    cout << "WARNING: NO EXCEPTION THROWN" << endl;
   }
-  catch (const std::exception& e) {
-    std::cout << "Exception caught (as expected): " << e.what() << std::endl;
+  catch (const exception& e) {
+    cout << "Exception caught (as expected): " << e.what() << endl;
   }
 
-  std::string registerNameMMap(DEFAULT_MMAP_REGISTER);
-  std::cout << "Trying to read register: " << registerNameMMap << " with MemMap interface" << std::endl;
+  string registerNameMMap(DEFAULT_MMAP_REGISTER);
+  cout << "Trying to read register: " << registerNameMMap << " with MemMap interface" << endl;
   uhal::ValWord<uint32_t> ret_mmap;
 
   // MemMap read, it should go bad here..
   ret_mmap = hw_mmap->getNode(registerNameMMap).read();
   hw_mmap->dispatch();
-  std::cout << "Succesfully read register" << std::endl;
-  std::cout << "Value: 0x" << std::hex << ret_mmap.value() << std::endl; 
+  cout << "Succesfully read register" << endl;
+  cout << "Value: 0x" << hex << ret_mmap.value() << endl; 
 
   return 0;
 }
